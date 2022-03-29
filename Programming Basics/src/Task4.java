0fbbddc2dfdd8cf3edd5a620a807d2dc7381d752
EmilianoZolla <102536537+EmilianoZolla@users.noreply.github.com>
2022-03-29 11:55:16 +0300

    Adding Task4.

---
+++ Programming Basics/src/Task4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        System.out.println(a*2.54);
+    }
+}

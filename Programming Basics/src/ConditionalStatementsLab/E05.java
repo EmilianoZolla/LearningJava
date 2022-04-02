a628f34fa547b468eb665955c12691353d148a85
EmilianoZolla <102536537+EmilianoZolla@users.noreply.github.com>
2022-04-02 18:04:44 +0300

    Commit.

---
+++ Programming Basics/src/ConditionalStatementsLab/E05.java
@@ -0,0 +1,23 @@
+package ConditionalStatementsLab;
+
+import java.util.Scanner;
+
+public class E05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        if (number < 100) {
+            System.out.println("Less than 100");
+        } else if (number < 200) {
+            System.out.println("Between 100 and 200");
+        } else {
+            System.out.println("Greater than 200");
+        }
+    }
+}
+// ⦁	Число от 100 до 200
+// Да се напише програма, която чете цяло число, въведено от потребителя и проверява дали е под 100,
+// между 100 и 200 или над 200. Ако числото е:
+//⦁	под 100 отпечатайте: "Less than 100"
+//⦁	между 100 и 200 отпечатайте: "Between 100 and 200"
+//⦁	над 200 отпечатайте: "Greater than 200"

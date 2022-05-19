57d759e472bdd87d9c1c4dba03052e0d764c3379
EmilianoZolla <102536537+EmilianoZolla@users.noreply.github.com>
2022-05-19 21:44:27 +0300

    Commit.

---
+++ Programming Fundamentals/src/basicSyntaxLab/E09.java
@@ -0,0 +1,18 @@
+package basicSyntaxLab;
+
+import java.util.Scanner;
+
+public class E09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+        for (int i = 1; i <= number * 2 ; i+=2) {
+            System.out.println(i);
+            sum = sum + i;
+        }
+        System.out.printf("Sum: %d",sum);
+    }
+}

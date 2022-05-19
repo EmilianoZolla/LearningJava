55901065f13190739139e9d73222065ba1752c7b
EmilianoZolla <102536537+EmilianoZolla@users.noreply.github.com>
2022-05-19 21:19:41 +0300

    Commit.

---
+++ Programming Fundamentals/src/basicSyntaxLab/E06.java
@@ -0,0 +1,19 @@
+package basicSyntaxLab;
+
+import java.util.Scanner;
+
+public class E06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String country = scanner.nextLine();
+
+        if (country.equals("England") || country.equals("USA")){
+            System.out.println("English");
+        } else if (country.equals("Spain") || country.equals("Argentina") || country.equals("Mexico")) {
+            System.out.println("Spanish");
+        } else {
+            System.out.println("unknown");
+        }
+    }
+}

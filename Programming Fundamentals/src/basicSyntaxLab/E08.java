5f951d41e81dae45746127a2334d04f2d19c22d0
EmilianoZolla <102536537+EmilianoZolla@users.noreply.github.com>
2022-05-19 21:36:36 +0300

    Commit.

---
+++ Programming Fundamentals/src/basicSyntaxLab/E08.java
@@ -0,0 +1,9 @@
+package basicSyntaxLab;
+
+public class E08 {
+    public static void main(String[] args) {
+        for (int i = 3; i < 100 ; i+=3) {
+            System.out.println(i);
+        }
+    }
+}

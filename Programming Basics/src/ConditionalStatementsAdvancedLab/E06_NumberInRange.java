8404ebac42ccadf73704a0fc3a0289533d6a7fa7
EmilianoZolla <102536537+EmilianoZolla@users.noreply.github.com>
2022-05-23 22:10:28 +0300

    Adding E06 Number in range  in  Conditional Statements Advanced Lab

--- Programming Basics/src/ConditionalStatementsAdvancedLab/E06_NumberInRange.java
+++ Programming Basics/src/ConditionalStatementsAdvancedLab/E06_NumberInRange.java
@@ -6,7 +6,7 @@ public class E06_NumberInRange {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = Integer.parseInt(scanner.nextLine());
-        if (number >= ( 0-100 ) && number != 0 && number <= 100 ) {
+        if (number >= ( -100 ) && number != 0 && number <= 100 ) {
             System.out.println("Yes");
         } else {
             System.out.println("No");

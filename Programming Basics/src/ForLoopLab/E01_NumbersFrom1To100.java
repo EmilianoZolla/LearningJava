9d88f92684dbbba292664ed48e3ba2a0c74375b4
EmilianoZolla <102536537+EmilianoZolla@users.noreply.github.com>
2022-05-24 18:24:17 +0300

    Added package FroLoopLab and task E01 Numbers from 1 to 100

---
+++ Programming Basics/src/ForLoopLab/E01_NumbersFrom1To100.java
@@ -0,0 +1,9 @@
+package ForLoopLab;
+
+public class E01_NumbersFrom1To100 {
+    public static void main(String[] args) {
+        for (int number = 1; number <= 100; ++number ){
+            System.out.println(number);
+        }
+    }
+}

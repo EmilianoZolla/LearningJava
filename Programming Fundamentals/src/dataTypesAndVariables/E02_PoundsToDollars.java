9512e747cd9adc4c4f123a55a1469088326f16f7
EmilianoZolla <102536537+EmilianoZolla@users.noreply.github.com>
2022-05-25 22:38:31 +0300

    Added task E02 Pounds to Dollars in package dataTypesAndVariables

---
+++ Programming Fundamentals/src/dataTypesAndVariables/E02_PoundsToDollars.java
@@ -0,0 +1,15 @@
+package dataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class E02_PoundsToDollars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        float pound = Float.parseFloat(scanner.nextLine());
+
+        float dollars = pound * 1.36f;
+
+        System.out.printf("%.3f",dollars);
+    }
+}

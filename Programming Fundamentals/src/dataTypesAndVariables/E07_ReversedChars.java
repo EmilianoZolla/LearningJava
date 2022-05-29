b72394af3dbf92e23ff004c0594391e5eb4e6541
EmilianoZolla <102536537+EmilianoZolla@users.noreply.github.com>
2022-05-29 11:35:59 +0300

    Added task E06 Chars to String in package dataTypesAndVariables in Programming Fundamentals module.

---
+++ Programming Fundamentals/src/dataTypesAndVariables/E07_ReversedChars.java
@@ -0,0 +1,15 @@
+package dataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class E07_ReversedChars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char lineOne = scanner.nextLine().charAt(0);
+        char lineTwo = scanner.nextLine().charAt(0);
+        char lineThree = scanner.nextLine().charAt(0);
+
+        System.out.printf("%c %c %c", lineThree, lineTwo, lineOne);
+    }
+}

0cac0d7c96d8bee1235afd12a85b78132514709c
EmilianoZolla <102536537+EmilianoZolla@users.noreply.github.com>
2022-05-29 11:32:47 +0300

    Added task E06 Chars to String in package dataTypesAndVariables in Programming Fundamentals module.

---
+++ Programming Fundamentals/src/dataTypesAndVariables/E06_CharsToString.java
@@ -0,0 +1,15 @@
+package dataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class E06_CharsToString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char lineOne = scanner.nextLine().charAt(0);
+        char lineTwo = scanner.nextLine().charAt(0);
+        char lineThree = scanner.nextLine().charAt(0);
+
+        System.out.printf("%c%c%c",lineOne, lineTwo, lineThree);
+    }
+}

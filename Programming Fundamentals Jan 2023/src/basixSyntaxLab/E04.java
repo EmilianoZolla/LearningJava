57b093f417a0ff3fc96837f1d1df74160ec83651
EmilianoZolla <102536537+EmilianoZolla@users.noreply.github.com>
2023-01-17 12:23:51 +0200

    Added tasks E04 in package basixSyntaxLab Programming Fundamentals Jan 2023 module.

--- Programming Fundamentals Jan 2023/src/basixSyntaxLab/E04.java
+++ Programming Fundamentals Jan 2023/src/basixSyntaxLab/E04.java
@@ -1,4 +1,22 @@
 package basixSyntaxLab;
 
+import java.util.Scanner;
+
 public class E04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int hour = Integer.parseInt(scanner.nextLine());
+        int minutes = Integer.parseInt(scanner.nextLine());
+
+        int totalMinutes = (hour * 60 ) + minutes + 30;
+        int afterHour = totalMinutes / 60;
+        int afterMinutes = totalMinutes % 60;
+
+        if (afterHour > 23) {
+            afterHour = 0;
+        }
+        System.out.printf("%d:%02d",afterHour,afterMinutes);
+
+    }
 }

708ffa5861680f3bd9786ecf514a2dbb94df064f
EmilianoZolla <102536537+EmilianoZolla@users.noreply.github.com>
2023-01-17 19:14:24 +0200

    Added task E07 to basicSyntaxLab package in Programming Fundamentals Jan 2023 Module

---
+++ Programming Fundamentals Jan 2023/src/basicSyntaxLab/E08.java
@@ -0,0 +1,9 @@
+package basicSyntaxLab;
+
+public class E08 {
+    public static void main(String[] args) {
+        for (int i = 3; i < 100; i=i+3) {
+            System.out.println(i);
+        }
+    }
+}

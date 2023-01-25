431820dfbe1167d5df233a054e5d87623ab9ddf5
EmilianoZolla <102536537+EmilianoZolla@users.noreply.github.com>
2023-01-25 13:18:25 +0200

    Added task E10 to basicSyntaxLab package in Programming Fundamentals Jan 2023 Module

---
+++ Programming Fundamentals Jan 2023/src/basicSyntaxLab/E10.java
@@ -0,0 +1,16 @@
+package basicSyntaxLab;
+
+import java.util.Scanner;
+
+public class E10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <=10 ; i++) {
+            int result = number * i;
+            System.out.printf("%d X %d = %d%n", number, i, result);
+
+        }
+    }
+}

646114ea6230e4ec831f887ae6bc9148eaabf73b
EmilianoZolla <102536537+EmilianoZolla@users.noreply.github.com>
2023-01-25 13:12:27 +0200

    Added task E09 (reworked) to basicSyntaxLab package in Programming Fundamentals Jan 2023 Module

--- Programming Fundamentals Jan 2023/src/basicSyntaxLab/E09.java
+++ Programming Fundamentals Jan 2023/src/basicSyntaxLab/E09.java
@@ -6,13 +6,14 @@ public class E09 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
-        int number = 0;
         int sum =0;
-        for (int i = 1; i <= n ; i++) {
+        for (int i = 0; i <= n ; i++) {
             if (i % 2 != 0){
                 System.out.println(i);
                 sum = sum+i;
+                n++;
             }
+
         }
         System.out.printf("Sum: %d",sum);
     }

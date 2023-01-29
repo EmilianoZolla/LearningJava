206967e53ecfd5505748f74e5fffe20f5cfc2248
EmilianoZolla <102536537+EmilianoZolla@users.noreply.github.com>
2023-01-29 12:59:15 +0200

    Added task E11 to basicSyntaxExe package in Programming Fundamentals Jan 2023 Module

--- Programming Fundamentals Jan 2023/src/basicSyntaxExe/E11.java
+++ Programming Fundamentals Jan 2023/src/basicSyntaxExe/E11.java
@@ -1,4 +1,36 @@
 package basicSyntaxExe;
 
+import java.util.Scanner;
+
 public class E11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int gamesLost = Integer.parseInt(scanner.nextLine());
+        double headsetPrice = Double.parseDouble(scanner.nextLine());
+        double mousePrice = Double.parseDouble(scanner.nextLine());
+        double keyboardPrice = Double.parseDouble(scanner.nextLine());
+        double displayPrice = Double.parseDouble(scanner.nextLine());
+
+        int headsetTrashed = 0;
+        int mouseTrashed = 0;
+        int keyboardTrashed = 0;
+        int displayTrashed = 0;
+        for (int gameLostCounter = 1; gameLostCounter <= gamesLost; gameLostCounter++) {
+            if (gameLostCounter % 2 == 0) {
+                headsetTrashed++;
+            }
+            if (gameLostCounter % 3 == 0){
+                mouseTrashed++;
+            }
+            if (gameLostCounter % 6 == 0){
+                keyboardTrashed++;
+                if (keyboardTrashed % 2 == 0){
+                    displayTrashed++;
+                }
+
+            }
+        }
+        double totalExpenses = (headsetPrice * headsetTrashed) + (mousePrice * mouseTrashed) + (keyboardPrice * keyboardTrashed) + (displayPrice * displayTrashed);
+        System.out.printf("Rage expenses: %.2f lv.", totalExpenses);
+    }
 }
